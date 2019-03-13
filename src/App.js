5b8e95fe78487e8caee7eed6eea52a388a9fce84
hempy <hempy.scrymetto@gmail.com>
2019-03-13 11:51:38 +0300

    checkbox done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react';
-import  'typeface-roboto';
+import React from 'react';
+import 'typeface-roboto';
 import './App.css';
 import Checkbox from './Components/Checkbox/Checkbox'
 import Greeting from './Components/Greeting'
 import Motivation from './Components/Motivation'
 import RandomWord from './Components/words/RandomWord'
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div>
                 <Greeting className="Greeting"/>
+
                 <div className="Content">
                     <div className="todo-list">
                         <h2>Tasks for today:</h2>
                         <Checkbox/>
                     </div>
+
                     <div>
                         <RandomWord/>
                     </div>
                 </div>
-                <div>
-                    <Motivation/>
-                </div>
 
+                <footer>
+                    <Motivation/>
+                </footer>
             </div>
+
         );
     }
 }
